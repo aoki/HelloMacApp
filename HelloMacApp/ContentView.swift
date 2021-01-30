83d17131d10f671d98a2c1d0e5125dfa3b95d1ff
aoki <ringohub@gmail.com>
2021-01-30 15:31:26 +0900

    Add count up

--- HelloMacApp/ContentView.swift
+++ HelloMacApp/ContentView.swift
@@ -1,9 +1,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var count: Int = 0
+    
+    func countUp() {
+        count += 1;
+    }
+    
     var body: some View {
         Text("Hello, world!")
             .padding()
+        HStack {
+            Text("\(count)")
+            Button(action: {self.countUp()}) {
+                Text("Count Up")
+            }
+        }.padding(.bottom)
     }
 }
 
